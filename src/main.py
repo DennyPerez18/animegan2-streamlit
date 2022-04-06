e0971d7f10d54c27d7b7de8b1584645cc7e82d70
Denny Perez <denny.perez23@gmail.com>
2022-04-05 21:34:52 -0400

    Cache torchhub repos directly with st.cache

--- src/main.py
+++ src/main.py
@@ -2,34 +2,45 @@ import torch
 import streamlit as st
 from PIL import Image
 
-from utils import create_side_by_side_image, save_pil_image_as_str
+from utils import create_side_by_side_image, save_pil_image_as_bytes
 
 
-REPO = "bryandlee/animegan2-pytorch:main"
-face2paint = torch.hub.load(REPO, "face2paint", size=512)
-
-trained_models_names = [
+TRAINED_MODELS_NAMES = [
     "celeba_distill",
     "face_paint_512_v1",
     "face_paint_512_v2",
     "paprika",
 ]
-trained_models = {
-    name: torch.hub.load(REPO, "generator", pretrained=name)
-    for name in trained_models_names
-}
+
+# This is not really necessary, since
+# torchhub already caches the models, but
+# the app logs at least will be cleaner.
+@st.cache(suppress_st_warning=True)
+def torchhub_repos_cache(models_names):
+    REPO = "bryandlee/animegan2-pytorch:main"
+    face2paint = torch.hub.load(REPO, "face2paint", size=512)
+    trained_models = {
+        name: torch.hub.load(REPO, "generator", pretrained=name)
+        for name in models_names
+    }
+    return face2paint, trained_models
+
+
+face2paint, trained_models = torchhub_repos_cache(TRAINED_MODELS_NAMES)
 
 st.title("AnimeGANv2")
 
 selected_model = st.sidebar.selectbox(
-    "Trained model to use:", trained_models_names, 2)
+    "Trained model to use:",
+    TRAINED_MODELS_NAMES, 2
+)
 
 img_file = st.file_uploader("Upload an image ( Recommended 512x512 ):")
 
+# todo: Add loading bar...
 if img_file is not None:
-    # todo: Add loading bar...
-
     original_img = Image.open(img_file).convert("RGB")
+
     model = trained_models[selected_model]
     trained_img = face2paint(model, original_img)
 
